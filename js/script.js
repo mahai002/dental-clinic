57e549d640fb6c41ff60d08edd72ad49e9421050
Maruf <maruf.zakirov.20042004@gmail.com>
2022-01-13 20:15:10 +0500

    R

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
 window.addEventListener('DOMContentLoaded',()=>{
-    let clientSlider = new Swiper('.client__slider',{
-        wrapperClass: 'client__wrapper',
-        slideClass: 'client__slide',
-        slidesPerView: 1,
-        grabCursor: true,
-        slidesPerGroup: 1,
-        pagination: {
-            el: '.client__pagination',
-            type: 'bullets',
-            bulletClass: 'client__pagination-bullet',
-            bulletActiveClass: 'client__pagination-bullet-active',
-          },
-        breakpoints: {
-            550: {
-                slidesPerView: 3,
-                spaceBetween: 50,
-                slidesPerGroup: 3,
+    if (document.querySelector('.client__slider')) {
+        let clientSlider = new Swiper('.client__slider',{
+            wrapperClass: 'client__wrapper',
+            slideClass: 'client__slide',
+            slidesPerView: 1,
+            grabCursor: true,
+            slidesPerGroup: 1,
+            pagination: {
+                el: '.client__pagination',
+                type: 'bullets',
+                bulletClass: 'client__pagination-bullet',
+                bulletActiveClass: 'client__pagination-bullet-active',
+              },
+            breakpoints: {
+                550: {
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                    slidesPerGroup: 3,
+                }
             }
-        }
-
-    });
+    
+        });   
+    }
     let shareWidgies = document.querySelectorAll('.share-widget:not(.full) .share-widget-sub');
     shareWidgies.forEach(el => {
         el.addEventListener('click', function () {
@@ -30,24 +32,27 @@ window.addEventListener('DOMContentLoaded',()=>{
             }
         });
     });
-    let modalSlider = new Swiper('.modal__slider',{
-        wrapperClass: 'modal__slider-wrapper',
-        slideClass: 'modal__slide',
-        slideActiveClass: 'modal__slide-active',
-        slidesPerView: 2,
-        grabCursor: true,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 3000,
-        },
-        breakpoints: {
-            728: {
-                slidesPerView: 3,
-            }
-        },
-        centeredSlides: true
-    });
-
-    lightGallery(document.getElementById('lightgallery')); 
+    if (document.querySelector('.modal__slider')) {
+        let modalSlider = new Swiper('.modal__slider',{
+            wrapperClass: 'modal__slider-wrapper',
+            slideClass: 'modal__slide',
+            slideActiveClass: 'modal__slide-active',
+            slidesPerView: 2,
+            grabCursor: true,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 3000,
+            },
+            breakpoints: {
+                728: {
+                    slidesPerView: 3,
+                }
+            },
+            centeredSlides: true
+        });   
+    }
+    if (document.getElementById('lightgallery')) {
+        lightGallery(document.getElementById('lightgallery')); 
+    }
 
 })
